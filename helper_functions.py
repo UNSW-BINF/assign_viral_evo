f56514a09353093e9c41fe50c5c04f31fa8f9aad
MartinSpendl <martin.spendl@gmail.com>
2023-11-22 10:17:37 +0100

    improved instructions

--- helper_functions.py
+++ helper_functions.py
@@ -1,20 +1,22 @@
 def jukes_cantor(reference_sequence: str, distant_sequence: str) -> float:
     """The Jukes-Cantor correction for estimating genetic distances
-    calculated with Hamming distance
+    calculated with Hamming distance.
+    Should return genetic distance with the same unit as if not corrected.
 
     Parameters
     ----------
     referene_sequence: str
         A string of nucleotides in a sequence used as a reference
-        in an alignment with other (i.e. ATGGC-TAG)
+        in an alignment with other (e.g. AGGT-GA)
     distant_sequence: str
         A string of nucleotides in a sequence after the alignment
-        with a reference (i.e. ATG-CTTAG)
+        with a reference (e.g. AGC-AGA)
 
     Returns
     -------
     float
         The Jukes-Cantor corrected genetic distance using Hamming distance.
+        For example 1.109.
 
     """
     raise NotImplementedError()
@@ -22,21 +24,23 @@ def jukes_cantor(reference_sequence: str, distant_sequence: str) -> float:
 
 def kimura_two_parameter(reference_sequence: str, distant_sequence: str) -> float:
     """The Kimura Two Parameter correction for estimating genetic distances
-    calculated with Hamming distance
+    calculated with Hamming distance.
+    Should return genetic distance with the same unit as if not corrected.
 
     Parameters
     ----------
     referene_sequence: str
         A string of nucleotides in a sequence used as a reference
-        in an alignment with other (i.e. ATGGC-TAG)
+        in an alignment with other (e.g. AGGT-GA)
     distant_sequence: str
         A string of nucleotides in a sequence after the alignment
-        with a reference (i.e. ATG-CTTAG)
+        with a reference (e.g. AGC-AGA)
 
     Returns
     -------
     float
         The Kimura corrected genetic distance using Hamming distance.
+        For example 1.128.
 
     """
     raise NotImplementedError()
